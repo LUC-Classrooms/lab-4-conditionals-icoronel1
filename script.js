ac482c9f4d15b03849d8f7258b977e7def0f3dff
Isa Coronel <156938989+icoronel1@users.noreply.github.com>
2024-02-02 16:53:15 +0000

    Lab 4 Complete; Extra Credit

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+var x, y, size
+
 function setup() {
   createCanvas(320, 240);
 }
 function draw() {
   background(200);
-var x = width/2
-var y = height/2
-var size = 100
   
   // check to condition of the mouse
  if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
    fill("blue");
+   x = width/4
+   size = 150
   }
   else { 
     // otherwise do this:
    fill("yellow");
+   x = width/2
+   y = height/2
+   size = 100
   }
   ellipse(x, y, size, size);
   
